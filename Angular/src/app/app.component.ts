bfd93d45ed9d72786094f6a6ffcafe6fb0981345
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 11:41:22 +0300

    chore(angular): ng update 18 → 19

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -3,9 +3,10 @@ import { DxPieChartTypes } from 'devextreme-angular/ui/pie-chart';
 import { Billionaire } from './app.types';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   billionaires: Billionaire[] = [
