7b20728883ee8e30199ac6ddfcc6e99c971beb87
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 14:29:30 +0300

    Update Angular to v22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { DxPieChartTypes } from 'devextreme-angular/ui/pie-chart';
 import { Billionaire } from './app.types';
 
@@ -8,6 +8,7 @@ import { DxPieChartModule } from 'devextreme-angular/ui/pie-chart';
     selector: 'app-root',
     imports: [DxPieChartModule],
     templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.Eager,
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
